d0825deb4e72840c72b43cdfd2bceb19d1bb1da1
Philip Hayes <philip@deewhy.ie>
2023-04-07 09:32:11 -0400

    Added docker build

--- src/main/java/org/jboss/helloworld/HelloWorld.java
+++ src/main/java/org/jboss/helloworld/HelloWorld.java
@@ -18,12 +18,12 @@ package org.jboss.helloworld;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 /**
  * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
